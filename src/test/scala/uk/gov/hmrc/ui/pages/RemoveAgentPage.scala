4bbabcab74dbd675684a1a4cb1ea92f2ea348df5
Philip Chukwuma <238522868+philipchukwuma@users.noreply.github.com>
2025-11-17 12:31:14 +0000

    Modified Remove Agent page title

--- src/test/scala/uk/gov/hmrc/ui/pages/RemoveAgentPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/RemoveAgentPage.scala
@@ -24,5 +24,5 @@ object RemoveAgentPage extends BasePage {
   val no: String  = "#value-2"
 
   override def pageTitle: String =
-    "Find your UK address - Find the agent’s address – Agent details - GOV.UK"
+    "Are you sure you want to remove this agent? – Agent details - Stamp Taxes Online - GOV.UK"
 }
